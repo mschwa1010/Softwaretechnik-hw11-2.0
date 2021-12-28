adffe6cfee75b29e50bb3cb3f41c8c364020f126
mschwa1010 <80753072+mschwa1010@users.noreply.github.com>
2021-12-28 16:57:19 +0100

    init commit

---
+++ src/main/java/com/softwaretechnik/SoftwaretechnikHw112Application.java
@@ -0,0 +1,13 @@
+package com.softwaretechnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwaretechnikHw112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwaretechnikHw112Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwaretechnik/SoftwaretechnikHw112ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwaretechnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwaretechnikHw112ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
